ccc1901bd1ded53220c69667cd2f9f03280eb127
Yeli Bengochea <yelibengochea@gmail.com>
2023-05-02 12:41:14 +0200

    add info functions

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -6,13 +6,15 @@ function greet(){
 // invoke the function 
 greet();
 
+
 // function expression
 const speak = function(){
   console.log('good day!');
 };
 
+// invoking the function
 speak();
-speak();
+
 
 function bye(){
   console.log('goodbye dear');
@@ -20,4 +22,22 @@ function bye(){
 
 bye();
 greet();
+
+// // pass value in a function. we create variables name and time in the function 
+// const hello = function(name, time){
+//   console.log(`hello, have a great ${time} ${name}`);
+// };
+
+// hello('mario', 'morning');
+
+
+// we add default paramenters in case we dont have value for the variables created in the f
+const hello = function(name = 'Luigi', time = 'afternoon'){
+  console.log(`hello, have a great ${time} ${name}`);
+};
+
+hello('shaun');
+
+
+
 
