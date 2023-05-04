c581560028dac39274e9e3248e933db6332532df
Yeli Bengochea <yelibengochea@gmail.com>
2023-05-04 13:18:50 +0200

    new update

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -14,11 +14,14 @@ console.log('area is:', area);
 // practise arrow functions
 
 // const greet = function(){
-//   return 'hello, world';
+//   return 'hello, my darling';
 // }
 
-const greet = () => 'hello, world';
+const greet = () => 'hello, my darling';
+const result = greet();
+console.log(result);
 
+//another exemple for functions with an array of parameter and values
 // const bill = function(products, tax){
 //   let total = 0;
 //   for(let i = 0; i < products.length; i++){
@@ -27,6 +30,8 @@ const greet = () => 'hello, world';
 //   return total;
 // }
 
+
+//version of the arrow function above
 const bill = (products, tax) => {
   let total = 0;
   for(let i = 0; i < products.length; i++){
@@ -35,5 +40,55 @@ const bill = (products, tax) => {
   return total;
 }
 
-console.log(greet());
 console.log(bill([10,15,30], 0.2));
+
+
+//rappel
+
+const bye = () => 'Bye bye team!';
+let speakOne = bye();
+console.log(speakOne);
+
+// methods 
+const surname = 'Bengochea';
+
+// surname.toUpperCase()
+let speakTwo = surname.toUpperCase();
+console.log(speakTwo);
+
+
+
+//callback function exemple
+
+
+const myFunc = (callbackFunc) => {
+  //do something
+  let value = 50;
+  callbackFunc(value);
+};
+
+myFunc(value => {
+  //do something
+  console.log(value);
+});
+
+
+// callback & foreach
+
+
+let people = ['mario', 'luigi', 'maria', 'lucio', 'ryu', 'shaun'];
+
+people.forEach(function(person){
+  console.log(person);
+});
+
+//converted into arrow function
+
+people.forEach(person => {
+  console.log(person);
+});
+
+
+
+
+
